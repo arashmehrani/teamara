b940b84804a26b2f5232dc2c7aa74cfbcbb1bcfc
arashmehrani <mehrani@protonmail.com>
2022-02-06 11:17:02 +0330

    fix

---
+++ Modules/Media/Database/Migrations/2022_02_06_072747_create_media_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMediaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media', function (Blueprint $table) {
+            $table->id();
+            $table->string('path');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('alt')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media');
+    }
+}
