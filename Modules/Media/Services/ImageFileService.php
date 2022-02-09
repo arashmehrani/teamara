34d8cac4f96a3909f6e3a3fb8297876263610577
arashmehrani <mehrani@protonmail.com>
2022-02-09 16:41:03 +0330

    fix

--- Modules/Media/Services/ImageFileService.php
+++ Modules/Media/Services/ImageFileService.php
@@ -10,13 +10,13 @@ class ImageFileService
 
         $name = uniqid();
         $extension = $file->getClientOriginalExtension();
-        $dir = 'public/uploads/images';
+        $dir = 'uploads/images/';
         $year_folder = $dir . date("Y");
         $month_folder = $year_folder . '/' . date("m");
         $dir = $month_folder;
-        !file_exists($year_folder) && mkdir($year_folder, 0777);
-        !file_exists($month_folder) && mkdir($month_folder, 0777);
-        $file->move(base_path($dir), $name . '.' . $extension);
+        !file_exists($year_folder) && mkdir($year_folder, 0777,true);
+        !file_exists($month_folder) && mkdir($month_folder, 0777,true);
+        $file->move(public_path($dir), $name . '.' . $extension);
         $path = $dir . '\\' . $name . '.' . $extension;
 
     }
