47e5460f5ceb6418882f7fb6170bbf7c83b3c6a1
arashmehrani <mehrani@protonmail.com>
2022-02-09 15:01:18 +0330

    fix

--- Modules/Media/Services/ImageFileService.php
+++ Modules/Media/Services/ImageFileService.php
@@ -8,6 +8,11 @@ class ImageFileService
     public static function upload($file)
     {
 
+        $name = uniqid();
+        $extension = $file->getClientOriginalExtension();
+        $dir = 'app\public';
+        $file->move(storage_path($dir), $name . '.' . $extension);
+        $path = $dir . '\\' . $name . '.' . $extension;
     }
 
 }
