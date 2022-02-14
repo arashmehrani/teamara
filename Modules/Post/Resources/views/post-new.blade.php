e5c484371afc6e0849ed1d90ed2832d0fe133311
arashmehrani <mehrani@protonmail.com>
2022-02-14 16:55:40 +0330

    fix

--- Modules/Post/Resources/views/post-new.blade.php
+++ Modules/Post/Resources/views/post-new.blade.php
@@ -3,8 +3,8 @@
     <title>افزودن نوشته > {{config('app.name')}}</title>
 @endsection
 @section('css')
-    <script src="{{asset('CDN/admin/assets/plugins/ckeditor/ckeditor.js')}}" ></script>
-    <script src="{{asset('CDN/admin/assets/plugins/ckeditor/ckeditor-teamara.js')}}" ></script>
+    <script src="{{asset('CDN/admin/assets/plugins/ckeditor/ckeditor.js')}}"></script>
+    <script src="{{asset('CDN/admin/assets/plugins/ckeditor/ckeditor-teamara.js')}}"></script>
 @endsection
 @section('breadcrumb')
     <h2 class="main-content-title tx-22 mg-b-5"> نوشتهٔ تازه </h2>
@@ -21,7 +21,7 @@
                         </div>
                     </div>
                     <div class="card-body">
-                            <div id="editor"></div>
+                        <div id="editor"></div>
                     </div>
                 </form>
             </div>
@@ -29,14 +29,54 @@
 
         <div class="col-sm-12 col-md-3 grid-margin">
             <div class="card custom-card">
-                <div class="card-header border-bottom-0 pb-0">
-                    انتشار
-                </div>
                 <div class="card-body">
-                    محتوا
+                    <div class="d-flex mb-3">
+                        <div class="">
+                            <a href="#" class="btn btn-sm btn-outline-dark">ذخیره پیش‌نویس</a>
+                        </div>
+                        <div class="mr-auto my-auto">
+                            <a href="#" class="btn btn-sm btn-outline-dark">پیش ‌نمایش</a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <span>وضعیت: </span><a href="#">پیش نویس</a>
+                    </div>
+                    <div class="mt-2">
+                        <span>قابلیت مشاهده: </span><a href="#">عمومی</a>
+                    </div>
+
+                    <div class="d-flex mt-3">
+                        <div class="mr-auto my-auto">
+                            <a class="btn btn-primary" href="#">انتشار</a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
+
+
+            <div class="card custom-card">
+                <div class="card-body">
+                    دسته بندی ها
+                </div>
+            </div>
+
+            <div class="card custom-card">
+                <div class="card-body">
+                    برچسب ها
+                </div>
+            </div>
+
+            <div class="card custom-card">
+                <div class="card-body">
+                    تصویر اصلی
+                </div>
+            </div>
+
+
         </div><!-- COL END -->
+
 
     </div>
 
@@ -44,9 +84,9 @@
 @section('javaScript')
     <script>
         ClassicEditor
-            .create( document.querySelector( '#editor' ) )
-            .catch( error => {
-                console.error( error );
-            } );
+            .create(document.querySelector('#editor'))
+            .catch(error => {
+                console.error(error);
+            });
     </script>
 @endsection
