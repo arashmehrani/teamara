0cce5af02ac1a042329801d42a3384b435ea6b2a
arashmehrani <mehrani@protonmail.com>
2022-02-16 15:04:06 +0330

    fix

---
+++ Modules/Auth/Http/Livewire/Password.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Auth\Http\Livewire;
+
+use Livewire\Component;
+
+class Password extends Component
+{
+    public function render()
+    {
+        return view('auth::livewire.password');
+    }
+}
